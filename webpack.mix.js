59f123004790caba7765b2b15a400ce7e79d8562
Dalton <daltonfiggdavies@gmail.com>
2026-07-07 11:20:10 +0100

    Initial project setup

---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+import mix from "laravel-mix";
+import tailwindcss from "@tailwindcss/postcss";
+
+mix.webpackConfig({
+  watchOptions: {
+    ignored: /node_modules|static|mix-manifest.json/,
+  },
+});
+
+mix
+  .js("src/js/app.js", "static/js")
+  .postCss("src/css/app.pcss", "static/css", [tailwindcss]);
+
+mix.disableNotifications();
