12b87cf7ca5e8338bc27705d8e9d2c80fad67a14
Dalton <daltonfiggdavies@gmail.com>
2026-07-08 10:07:05 +0100

    Add coordinate to index conversion utilities

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,44 @@
-console.log("Hello World!");
+const ZERO_INDEXED_FILES = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+  e: 4,
+  f: 5,
+  g: 6,
+  h: 7,
+};
+
+const ZERO_INDEXED_RANKS = {
+  1: 0,
+  2: 1,
+  3: 2,
+  4: 3,
+  5: 4,
+  6: 5,
+  7: 6,
+  8: 7,
+};
+
+const algebraicCoordinateToIndex = (coordinate) => {
+  const [file, rank] = coordinate.split("");
+
+  const zeroIndexedFile = ZERO_INDEXED_FILES[file];
+  const zeroIndexedRank = ZERO_INDEXED_RANKS[rank];
+
+  const index = zeroIndexedRank * 8 + zeroIndexedFile;
+
+  return index;
+};
+
+const indexToAlgebraicCoordinate = (index) => {
+  const files = "abcdefgh";
+
+  const zeroIndexedFile = index % 8;
+  const zeroIndexedRank = Math.floor(index / 8);
+
+  const file = files[zeroIndexedFile];
+  const rank = zeroIndexedRank + 1;
+
+  return `${file}${rank}`;
+};
--- static/js/app.js
+++ static/js/app.js
@@ -21,7 +21,50 @@ __webpack_require__.r(__webpack_exports__);
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-console.log("Hello World!");
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+var ZERO_INDEXED_FILES = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+  e: 4,
+  f: 5,
+  g: 6,
+  h: 7
+};
+var ZERO_INDEXED_RANKS = {
+  1: 0,
+  2: 1,
+  3: 2,
+  4: 3,
+  5: 4,
+  6: 5,
+  7: 6,
+  8: 7
+};
+var algebraicCoordinateToIndex = function algebraicCoordinateToIndex(coordinate) {
+  var _coordinate$split = coordinate.split(""),
+    _coordinate$split2 = _slicedToArray(_coordinate$split, 2),
+    file = _coordinate$split2[0],
+    rank = _coordinate$split2[1];
+  var zeroIndexedFile = ZERO_INDEXED_FILES[file];
+  var zeroIndexedRank = ZERO_INDEXED_RANKS[rank];
+  var index = zeroIndexedRank * 8 + zeroIndexedFile;
+  return index;
+};
+var indexToAlgebraicCoordinate = function indexToAlgebraicCoordinate(index) {
+  var files = "abcdefgh";
+  var zeroIndexedFile = index % 8;
+  var zeroIndexedRank = Math.floor(index / 8);
+  var file = files[zeroIndexedFile];
+  var rank = zeroIndexedRank + 1;
+  return "".concat(file).concat(rank);
+};
 
 /***/ })
 
